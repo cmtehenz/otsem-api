014388fe9a89bfe896dfc149ad6edf5b9f9445e0
Gustavo Costa <cmtehenz@gmail.com>
2025-11-22 11:24:23 -0300

    ajuste cors

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
-import { PrismaService } from '../prisma/prisma.service';
+import { InterModule } from '../inter/inter.module';
+import { PrismaModule } from '../prisma/prisma.module'; // Adicione esta linha
 
 @Module({
+    imports: [InterModule, PrismaModule], // Adicione PrismaModule aqui
     controllers: [PaymentsController],
-    providers: [PaymentsService, PrismaService],
+    providers: [PaymentsService],
 })
 export class PaymentsModule { }
